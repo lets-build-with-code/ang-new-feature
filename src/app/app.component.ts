b08396ea89e2663f487d5c8e5f7ef9f1348e7668
DESKTOP-PO4U83T\sushi <sunil.ladhyal.sharma@gmail.com>
2024-07-21 15:56:45 +0530

    Angular 18 : @for - control flows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,37 @@
-import { Component,  } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'new-ang-feature';
-  num: number = 1;
+  title = 'ang-new-feature';
+  items = [
+    {id:1, name:"A"},
+    {id:1, name:"B"},
+  ]
+
+  ngOnInit() {
+
+    setTimeout(() => {
+      console.log({seTimeout : true});
+      this.items = [
+        {id:1, name:"A"},
+        {id:1, name:"B"},
+        {id:1, name:"C"},
+        {id:1, name:"D"},
+      ]
+    }, 5000);
+    
+  }
+  trackByid(index:any, items:any) {
+    console.log({trackBy :true,  index, items })
+    return index;
+
+  }
 }
