ccd14cebcd3c7db6d6e5e820d4a0601bbc91c26a
DESKTOP-PO4U83T\sushi <sunil.ladhyal.sharma@gmail.com>
2024-08-15 17:54:33 +0530

    Angular-18: Lazy loaded component with viewcontainer ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,23 @@ import { CommonModule } from '@angular/common';
 import { Component, ComponentRef, ElementRef, TemplateRef, viewChild, ViewChild, ViewContainerRef, ViewRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { DynamicComponent } from './dynamic/dynamic.component';
+// import { DynamicComponent } from './dynamic/dynamic.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, DynamicComponent],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 @ViewChild('viewContainer', {read: ViewContainerRef}) viewContainer !: ViewContainerRef;
-@ViewChild('templateContainer', {read: TemplateRef}) templateContainer !: 
-TemplateRef<any>;
 
 
-getStudentsDetails() {
-  const students = [
-    {rollno:1, name:'sun', course:'angular'},
-    {rollno:2, name:'sam', course:'angular'},
-    {rollno:3, name:'sid', course:'java'},
-  ];
 
-  this.viewContainer.createEmbeddedView(this.templateContainer, {students})
+async loadComponent() {
+ const {DynamicComponent} = await import('./dynamic/dynamic.component');
+  this.viewContainer.createComponent(DynamicComponent)
 }
 
 }
